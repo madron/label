d41e4662d4dd6c35ea3b1ccd3bc8383efd2a1a59
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2023-11-06 10:07:17 +0100

    restart from scratch

--- label/app.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-from label import storage
-
-
-class App:
-    def __call__(self, page: ft.Page):
-        self.page = page
-        self.page.add(ft.SafeArea(ft.Text('Label')))
-        self.storage_path = storage.get_storage_path(self.page)
--- main.py
+++
@@ -1,5 +0,0 @@
-import flet as ft
-from label.app import App
-
-
-ft.app(App())
